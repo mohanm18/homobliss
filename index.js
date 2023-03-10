54c6af8466c3a43c9c547505329c30234ff863a6
Mohan Mondhe <mohanmondhe30@gmail.com>
2023-03-11 01:09:08 +0530

    .
    .

---
+++ index.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector('.navbar-toggle');
+const navbarLinks = document.querySelector('.navbar-links');
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+});
